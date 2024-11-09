1757ce52ecf0014b1afcb1ad93e2f8e07192c5c5
Jake Cheng <chengjiajun42@gmail.com>
2024-11-09 15:12:00 -0800

    please work

--- gold/rasterizer.c
+++ gold/rasterizer.c
@@ -14,6 +14,14 @@
 int min(int a, int b)
 {
   // START CODE HERE
+  if (a < b)
+  {
+    return a;
+  }
+  else
+  {
+    return b;
+  }
   // END CODE HERE
 }
 
@@ -24,6 +32,14 @@ int min(int a, int b)
 int max(int a, int b)
 {
   // START CODE HERE
+  if (a > b)
+  {
+    return a;
+  }
+  else
+  {
+    return b;
+  }
   // END CODE HERE
 }
 
@@ -34,6 +50,12 @@ int max(int a, int b)
 int floor_ss(int val, int r_shift, int ss_w_lg2)
 {
   // START CODE HERE
+  //r_shift represents the decimal point position
+  //ss_w_lg2 represents the number of bits to represent subsample width
+  int bits_cut = r_shift - ss_w_lg2;
+  int mask = ~((1 << bits_cut)-1);
+  return val & mask;
+  // return 0;
   // END CODE HERE
 }
 
@@ -45,19 +67,62 @@ int floor_ss(int val, int r_shift, int ss_w_lg2)
 BoundingBox get_bounding_box(Triangle triangle, Screen screen, Config config)
 {
   BoundingBox bbox;
-
+  // // init bbox to 0
+  bbox.lower_left.x = triangle.v[0].x;
+  bbox.lower_left.y = triangle.v[0].y;
+  bbox.upper_right.x = triangle.v[0].x;
+  bbox.upper_right.y = triangle.v[0].y;
+  bbox.valid = true;
   // START CODE HERE
   // initialize bounding box to first vertex
- 
+  int lowerleft_x = triangle.v[0].x;
+  int lowerleft_y = triangle.v[0].y;
+  int upperright_x = triangle.v[0].x;
+  int upperright_y = triangle.v[0].y;
+  // printf("triangle.v[0].x: %d\n", triangle.v[0].x);
+  // printf("triangle.v[0].y: %d\n", triangle.v[0].y);
+  // printf("triangle.v[1].x: %d\n", triangle.v[1].x);
+  // printf("triangle.v[1].y: %d\n", triangle.v[1].y);
+  // printf("triangle.v[2].x: %d\n", triangle.v[2].x);
+  // printf("triangle.v[2].y: %d\n", triangle.v[2].y);
   // iterate over remaining vertices
-
+  for (int i=0; i<3; i++) {
+    lowerleft_x = min(lowerleft_x, triangle.v[i].x);
+    lowerleft_y = min(lowerleft_y, triangle.v[i].y);
+    upperright_x = max(upperright_x, triangle.v[i].x);
+    upperright_y = max(upperright_y, triangle.v[i].y);  }
+  // printf("debug1 initial\n");
+  // printf("lowerleft_x: %d\n", lowerleft_x);
+  // printf("lowerleft_y: %d\n", lowerleft_y);
+  // printf("upperright_x: %d\n", upperright_x);
+  // printf("upperright_y: %d\n", upperright_y);
   // round down to subsample grid
-
+  bbox.lower_left.x = floor_ss(lowerleft_x, config.r_shift, config.ss_w_lg2);
+  bbox.lower_left.y = floor_ss(lowerleft_y, config.r_shift, config.ss_w_lg2);
+  bbox.upper_right.x = floor_ss(upperright_x, config.r_shift, config.ss_w_lg2);
+  bbox.upper_right.y = floor_ss(upperright_y, config.r_shift, config.ss_w_lg2);
+  // printf("debug2 after rounding\n");
+  // printf("lowerleft_x: %d\n", bbox.lower_left.x);
+  // printf("lowerleft_y: %d\n", bbox.lower_left.y);
+  // printf("upperright_x: %d\n", bbox.upper_right.x);
+  // printf("upperright_y: %d\n", bbox.upper_right.y);
   // clip to screen
-
-  // check if bbox is valid
-
-  // END CODE HERE
+  bbox.upper_right.y = min(screen.height, bbox.upper_right.y);
+  bbox.upper_right.x = min(screen.width, bbox.upper_right.x);
+  bbox.lower_left.y = max(0, bbox.lower_left.y);
+  bbox.lower_left.x = max(0, bbox.lower_left.x);
+  // printf("debug3 after clipping\n");
+  // printf("screen.width: %d\n", screen.width);
+  // printf("screen.height: %d\n", screen.height);
+  // printf("lowerleft_x: %d\n", bbox.lower_left.x);
+  // printf("lowerleft_y: %d\n", bbox.lower_left.y);
+  // printf("upperright_x: %d\n", bbox.upper_right.x);
+  // printf("upperright_y: %d\n", bbox.upper_right.y);
+  // // check if bbox is valid
+  bbox.valid = (bbox.upper_right.x > bbox.lower_left.x) && (bbox.upper_right.y > bbox.lower_left.y);
+  // // END CODE HERE
+  // printf("debug4\n");
+  // printf("valid: %d\n", bbox.valid);
   return bbox;
 }
 
@@ -72,6 +137,18 @@ bool sample_test(Triangle triangle, Sample sample)
   bool isHit;
 
   // START CODE HERE
+  int x0 = triangle.v[0].x - sample.x;
+  int y0 = triangle.v[0].y - sample.y;
+  int x1 = triangle.v[1].x - sample.x;
+  int y1 = triangle.v[1].y - sample.y;
+  int x2 = triangle.v[2].x - sample.x;
+  int y2 = triangle.v[2].y - sample.y;
+
+  int cross0 = x0 * y1 - x1 * y0;
+  int cross1 = x1 * y2 - x2 * y1;
+  int cross2 = x2 * y0 - x0 * y2;
+
+  isHit = (cross0 <= 0 && cross1 <= 0 && cross2 <= 0);
   // END CODE HERE
 
   return isHit;
